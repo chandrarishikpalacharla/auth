cc1019cb4f723861635cb55a193f603073f19454
Chandra rishik Palacharla <chandrarishikpalacharla@Chandras-Laptop.local>
2023-11-15 17:09:27 +0530

    verification email changes

--- lib/verifyemailpage.dart
+++ lib/verifyemailpage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:photo/homePage.dart';
@@ -15,12 +16,14 @@ class VerifyEmailPage extends StatefulWidget {
 class _VerifyEmailPageState extends State<VerifyEmailPage> {
   bool isEmailVerifed = false;
   bool canResendEmail = false;
+  bool isMounted = false; 
   Timer? timer;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    isMounted = true;
     isEmailVerifed  =FirebaseAuth.instance.currentUser!.emailVerified;
 
     if(!isEmailVerifed){
@@ -34,14 +37,17 @@ class _VerifyEmailPageState extends State<VerifyEmailPage> {
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+    isMounted = false; 
     timer?.cancel();
   }
 
   Future checkEmailVerified() async{
     await FirebaseAuth.instance.currentUser!.reload();
+    if(isMounted){
     setState(() {
       isEmailVerifed = FirebaseAuth.instance.currentUser!.emailVerified;
     });
+    }
     if(isEmailVerifed)timer?.cancel();
   }
   
@@ -49,9 +55,13 @@ class _VerifyEmailPageState extends State<VerifyEmailPage> {
     try{
       final user = FirebaseAuth.instance.currentUser!;
     await user.sendEmailVerification();
+    if(isMounted){
     setState(()=>canResendEmail = false);
+    }
     await Future.delayed(Duration(seconds: 5));
+    if(isMounted){
     setState(()=>canResendEmail = true);
+    }
     }
     catch(e){
       Utils.showSnackBar(e.toString());
@@ -61,6 +71,12 @@ class _VerifyEmailPageState extends State<VerifyEmailPage> {
   Future<void> deleteCurrentUserAndNavigate(BuildContext context) async {
     try {
       final user = FirebaseAuth.instance.currentUser!;
+
+      // Get the user ID
+      final uid = user.uid;
+
+      await deleteUserData(uid);
+
       await user.delete();
 
       // Sign out the user
@@ -73,6 +89,15 @@ class _VerifyEmailPageState extends State<VerifyEmailPage> {
     //     (Route<dynamic> route) => false, // This prevents going back to the previous screens
     //   );
      } catch (e) {
+      Utils.showSnackBar(e.toString());
+    }
+  }
+
+  Future<void> deleteUserData(String uid) async {
+    try {
+      // Delete user data from Firestore
+      await FirebaseFirestore.instance.collection('userPosts').doc(uid).delete();
+    } catch (e) {
       Utils.showSnackBar(e.toString());
     }
   }
